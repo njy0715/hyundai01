2f290a96aa6a4ae2f74eca7b93af0980a0690e37
njy0715 <103920374+njy0715@users.noreply.github.com>
2022-06-16 22:32:47 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,185 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <header className="App-header">
+
+      <div> 
+      <ul>
+       <li> &nbsp; &nbsp;  App 다운로드&nbsp; | &nbsp; </li>
+       <li> &nbsp;   개인 로그인 &nbsp; |  &nbsp;</li>
+       <li> &nbsp;  법인 로그인 &nbsp; |  &nbsp;</li>
+       <li> &nbsp;  회원가입 &nbsp; |  &nbsp;</li>
+       <li> &nbsp;   월드와이드 &nbsp; | &nbsp;</li>
+       <li> &nbsp;   LANGUAGE &nbsp;  | &nbsp; &nbsp;</li>
+       <li> &nbsp;   채용 &nbsp; | &nbsp; &nbsp;</li>
+       <li> &nbsp;   검색 &nbsp; &nbsp;</li>
+      </ul>
+       </div>
+
+       <br></br> <br></br> <br></br>
+
+        <div>
+      <ul>
+      <li><img src="./logo.jpg" alt="main" /> </li>
+       <li> &nbsp;&nbsp;모델&nbsp;&nbsp;&nbsp; </li>
+       <li> &nbsp;&nbsp;구매정보&nbsp;&nbsp;&nbsp; </li>
+       <li> &nbsp;&nbsp;멤버쉽&차량관리&nbsp;&nbsp;&nbsp; </li>
+       <li> &nbsp;&nbsp;이벤트&nbsp;&nbsp;&nbsp; </li>
+       <li> &nbsp;&nbsp;고객지원&nbsp;&nbsp;&nbsp; </li>
+       <li> &nbsp;&nbsp;브랜드&nbsp;&nbsp;&nbsp;&nbsp; </li>
+       <li> &nbsp;&nbsp;회사소개&nbsp;&nbsp;&nbsp; </li>
+       <li> &nbsp;&nbsp;<img src="./shop_text.jpg" alt="main" />&nbsp; </li>
+      </ul>   
+       </div>
+
+       <br></br> <br></br> <br></br>
+
+       <div>
+       <img src="./main.jpg" alt="main" />    
+       </div>
+
+       <div>     
+       <a>   
+            <br></br> <br></br> <br></br> <br></br> 
+            로그인 하시고 현대 자동차의 편리하고 다양한 맞춤형 서비스를 이용해 보세요.
+            &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<a href=''> 로그인 | 회원가입</a> </a>
+            <br></br> <br></br> <br></br> <br></br>
+       </div>
+
+       <div>
+       <br></br>
+         <a>신차 구매 정보  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  블루멤버스 및 차량 관리 정보</a>
+       <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br>
+       </div>
+
+       <div>
+         <h1>이달의 구매혜택</h1>  
+            <ul>
+              <li><img src="./mon1.jpg" alt="mon" /></li>
+              <li><img src="./mon2.jpg" alt="mon" /></li>
+              <li><img src="./mon3.jpg" alt="mon" /></li>
+              <li><img src="./mon4.jpg" alt="mon" /></li>
+            </ul>
+       </div>
+
+       <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br>
+       <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br>
+       <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br>
+       <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br>
+
+
+       <div>
+         <h1>차종 라인업</h1>
+         <a>승용 &nbsp; &nbsp; &nbsp; &nbsp; 
+            SUV &nbsp; &nbsp; &nbsp; &nbsp;    
+            MPV &nbsp; &nbsp; &nbsp; &nbsp;    
+            수소/전기차 &nbsp; &nbsp; &nbsp; &nbsp;    
+            N/N Line &nbsp; &nbsp; &nbsp; &nbsp;    
+            소형상용&택시</a>
+
+            <ul>
+              <li><img src="./line up1.webp" alt="main" /></li>
+              <li><img src="./line up2.webp" alt="main" /></li>
+              <li><img src="./line up3.webp" alt="main" /></li>
+              <li><img src="./line up4.webp" alt="main" /></li>
+            </ul>
+            <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> 
+            <ul>
+              <li><a><h3>그랜저</h3> 3,392만원~</a></li> 
+              <li><a><h3>그랜저 Hybrid</h3> 3,787만원~</a></li>
+              <li><a><h3>아반떼</h3> 1,866만원~</a></li>
+              <li><a><h3>아반떼 Hybrid</h3> 2,346만원~</a></li>
+              <li><a><h3>쏘나타</h3> 2,547만원</a></li>
+              
+            </ul>
+       </div>
+
+        
+         <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br>
+
+       <div>
+         <h2>아직 내 차를 결정하지 못했다면? &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;   상세모델검색 &nbsp; &nbsp; &nbsp; &nbsp; 내 차 추천받기</h2>  
+         <h3>원하는 모델을 찾아보세요.</h3>
+       </div>
+   
+         <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br>
+        
+
+       <div>
+         <h1>차량 탐색</h1>
+         <h2>판매량TOP3</h2> <a> 최근 한 달 동안 다른 사람들이 가장 많이 구매한 모델입니다. </a>
+         &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
+         <a>전체</a> &nbsp;&nbsp;|&nbsp;&nbsp; <a>2~30대</a> &nbsp;&nbsp;|&nbsp;&nbsp; <a>40대</a> &nbsp;&nbsp;|&nbsp;&nbsp; <a> 50대</a> 
+         <h2>1위</h2> 
+         <h2>그랜저</h2> <h4>Le Blanc(르블랑)</h4>
+         <a>엔진<h4>스마트스트림 가솔린 2.5</h4>표준연비 <h4>11.6km/l</h4></a>
+         <img src="./car1.webp" alt="main" />    
+         <br></br>
+         <h1>간편 할부계산기</h1>
+         <h4>할부 금액으로 맞춤형 그융 상품을 검색해보세요.</h4>
+
+         <h1>비교하기</h1>
+         <h4>빠르게 비교하여 원하는 차량을 찾아보세요.</h4>
+       </div>
+
+       <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br>
+
+       <div>
+          <h2>실제 차량을 확인하세요! &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;   시승신청 &nbsp; &nbsp; &nbsp; &nbsp; 판매/시승 네트워크 검색</h2>  
+          <h3>판매/시승 네트워크 검색을 통해 실제 차량을 확인해보세요.</h3>
+       </div>
+
+  <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br>
+
+       <div>
+         <h1>이벤트</h1>
+         <img src="./event1.jpg" alt="event1" />   
+         <h3>마이현대 차량 등록 고객 한정</h3>
+         <h2>카라이프 대리 서비스 1만원 할인!</h2>
+         <a> 2022-06-15 ~ 2022-12-31 </a>
+         <a href=''><h4>자세히보기</h4></a>
+         <br></br> <br></br> <br></br>
+         <img src="./event2.jpg" alt="event2" />   
+         <h3>마이현대 차량 등록 고객 한정</h3>
+         <h2>플러스친구 맺고 5,000p 적립의 행운을 잡으세요!</h2>
+         <a> 2022-06-15 ~ 2022-07-14 </a>
+         <a href=''><h4>자세히보기</h4></a>
+       </div>
+
+       <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br>
+       <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br>
+
+       <div>
+         <h1>현대자동차 소식</h1>
+         <a>#All</a> &nbsp; <a>#현대자동차</a> &nbsp; <a>#Hyundai_N</a> &nbsp; <a>#CASPER</a> &nbsp; <a>#TheFutureofMobility</a> 
+         <a>#about_hyundai</a> &nbsp; <a>#친환경</a> &nbsp; <a>#BTS</a> &nbsp; <a>#IONIQ</a>
+       </div>
+
+       <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br>
+       <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br>
+
+       <div>
+         <a>모바일 App</a> | <a>저작권안내</a> | <a>개인정보 처리방침</a> | <a>이용약관</a> <br></br>
+         <br></br>
+         <a>윤리경영 사이버 감사실</a> | <a>영상정보처리기기 운영관리방침</a> | <a>사이트맵</a> <br></br> 
+         <br></br>
+         <a>고객센터 : 080-600-6000(승용), 080-200-6000(상용)</a> <br></br>
+         <a>COPYRIGHT HYUNDAI MOTOR COMPANY.ALL RIGHTS RESERVED.</a> <br></br>
+       </div>
+
+ 
+
+       
+      </header>
+
+
+    </div>
+  );
+}
+
+export default App;
